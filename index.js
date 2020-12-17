3929e6abda077dff60834171ead320868b65ed5e
Shubham Mathur <shumathu@redhat.com>
2020-12-18 00:43:11 +0530

    refactor

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-const vscodeCommons = vscode.extensions.getExtension(
-    "redhat.vscode-commons"
-  );
-let context;
+import { Initialize } from "./initialize";
+// function initialize() {
+//     const vscodeCommons = vscode.extensions.getExtension(
+//         "redhat.vscode-commons"
+//       );
+//     let vscodeCommonsIsAlive = false;
+//     if (vscodeCommons?.isActive) {
+//         console.log("alice: redhat.vscode-commons is active");
+//         vscodeCommonsIsAlive = true;
+//     } else {
+//         console.log("alice: redhat.vscode-commons is not active");
+//         await vscodeCommons?.activate().then(
+//             function () {
+//                 console.log("alice: redhat.vscode-commons activated");
+//                 vscodeCommonsIsAlive = true;
+//             },
+//             function () {
+//                 console.log("alice: redhat.vscode-commons activation failed");
+//             }
+//         );
+//     }
+//   return vscodeCommonsIsAlive
+// }
 
-function initialize(context) {
-     context = context;
-    let vscodeCommonsIsAlive = false;
-    if (vscodeCommons?.isActive) {
-        console.log("alice: redhat.vscode-commons is active");
-        vscodeCommonsIsAlive = true;
-    } else {
-        console.log("alice: redhat.vscode-commons is not active");
-        await vscodeCommons?.activate().then(
-            function () {
-                console.log("alice: redhat.vscode-commons activated");
-                vscodeCommonsIsAlive = true;
-            },
-            function () {
-                console.log("alice: redhat.vscode-commons activation failed");
-            }
-        );
-    }
-  return vscodeCommonsIsAlive
-}
-
-function getTelemetryService(extensionIdentifier){
-    const vscodeCommonsAPI = vscodeCommons?.exports;
-    const telemetryService = vscodeCommonsAPI.getTelemetryService(
-        extensionIdentifier
-      );
-      context.subscriptions.push(telemetryService);
-      return telemetryService;
-}
-
+// function getTelemetryService(extensionIdentifier){
+//     const vscodeCommonsAPI = vscodeCommons?.exports;
+//     const telemetryService = vscodeCommonsAPI.getTelemetryService(
+//         extensionIdentifier
+//       );
+//     //   context.subscriptions.push(telemetryService);
+//       return telemetryService;
+// }
