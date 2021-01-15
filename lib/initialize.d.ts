7f147749d154080111ce44059509d6f7ab561a86
Shubham Mathur <shumathu@redhat.com>
2021-01-15 14:27:19 +0530

    ver change

--- lib/initialize.d.ts
+++
@@ -1,3 +0,0 @@
-import * as vscode from "vscode";
-export declare function initialize(): Promise<vscode.Extension<any> | undefined>;
-//# sourceMappingURL=initialize.d.ts.map
--- lib/setExtensionName.d.ts
+++
@@ -1,3 +0,0 @@
-export declare let clientExtensionName: string;
-export declare function setExtensionName(extensionName: string): Promise<void>;
-//# sourceMappingURL=setExtensionName.d.ts.map
