2c1f625c406c96833e3ceaf36086b03ec5fb7e69
Shubham Mathur <shumathu@redhat.com>
2021-02-08 12:38:56 +0530

    revert quotes

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,7 +4,7 @@ export let doLog: boolean = false;
 export namespace Logger {
   export function log(s: number | string | boolean | undefined): void {
     if (doLog) {
-      console.log("vscode-redhat-telemetry: " + s);
+      console.log('vscode-redhat-telemetry: ' + s);
     }
   }
 }
