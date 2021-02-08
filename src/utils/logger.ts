8cb30bd79cb91a0de42a47b72055ee0cae8d20e1
Shubham Mathur <shumathu@redhat.com>
2021-02-08 12:36:06 +0530

    Deactivate log

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,10 +1,10 @@
-export let doLog: boolean = true;
+export let doLog: boolean = false;
 
 // This exists only for testing purposes. Could delete later.
 export namespace Logger {
   export function log(s: number | string | boolean | undefined): void {
     if (doLog) {
-      console.log('vscode-redhat-telemetry: ' + s);
+      console.log("vscode-redhat-telemetry: " + s);
     }
   }
 }
