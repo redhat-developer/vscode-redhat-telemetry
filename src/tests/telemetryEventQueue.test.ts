740f23eb861b79442dbfb787a9d7944869e7232a
Shubham Mathur <shumathu@redhat.com>
2021-03-17 18:39:50 +0530

    Add test case setup

---
+++ src/tests/telemetryEventQueue.test.ts
@@ -0,0 +1,30 @@
+import { TelemetryEvent } from "../interfaces/telemetry";
+import {
+  TelemetryEventQueue,
+  MAX_QUEUE_SIZE,
+} from "../utils/telemetryEventQueue";
+import * as assert from "assert";
+
+let dummyEvent: TelemetryEvent = { name: "test" };
+
+//TODO Move to @redhat-developer/vscode-redhat-telemetry
+suite("Event Queue Test Suite", () => {
+  let queue = new TelemetryEventQueue();
+  test("should generate event queue", () => {
+    assert.strictEqual(typeof queue.events, typeof []);
+  });
+  test("should push element in event queue", () => {
+    queue.addEvent(dummyEvent);
+    assert.strictEqual(queue.events?.length, 1);
+  });
+  test("should test array limits", () => {
+    for (let index = 0; index < MAX_QUEUE_SIZE + 1; index++) {
+      queue.addEvent(dummyEvent);
+    }
+    assert.strictEqual(queue.events?.length, MAX_QUEUE_SIZE);
+  });
+  test("should destroy the queue", () => {
+    queue.emptyQueue();
+    assert.strictEqual(queue.events, undefined);
+  });
+});
