1545dac0e34ade8e3a244949082ac02599c525c6
Sorin Sbarnea <ssbarnea@redhat.com>
2023-01-04 13:09:45 +0000

    Add webpack testing
    - adds a minimal webpack script for testing ability to bundle
    - configure webpack to treat warnings as errors

---
+++ webpack.config.ts
@@ -0,0 +1,30 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+const WarningsToErrorsPlugin = require('warnings-to-errors-webpack-plugin');
+
+// in case you run into any typescript error when configuring `devServer`
+// import 'webpack-dev-server';
+
+const config: webpack.Configuration = {
+  mode: 'production',
+  entry: './test/index.ts',
+  externals: {
+    "vscode": "commonjs vscode",
+  },
+  target: "node", // vscode extensions run in a Node.js-context
+  node: {
+    __dirname: false, // leave the __dirname-behavior intact
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'foo.bundle.js',
+  },
+  plugins: [
+    new WarningsToErrorsPlugin(),
+  ],
+  stats: {
+    errorDetails: true,
+  }
+};
+
+export default config;
